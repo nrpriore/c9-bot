270d335c41c77edfa55e47c5322e6f1c2e3e4563
Nicholas Priore <nr.priore@gmail.com>
2020-10-09 21:08:46 -0400

    wip

--- database/routes/event_reg.js
+++ database/routes/event_reg.js
@@ -2,9 +2,10 @@ const db = require('./../index.js');
 
 
 exports.get = async (guild, event) => {
-	const { res } = await db.query('SELECT active_ind FROM event_reg WHERE guild_id = $1 AND event_name = $2', [guild, event]);
-	console.log(res.active_ind);
-	console.log(res[0].active_ind);
+	const { res } = await db.query('SELECT * FROM event_reg WHERE guild_id = $1 AND event_name = $2', [guild, event]);
+	console.log(res);
+	//console.log(res.active_ind);
+	//console.log(res[0].active_ind);
 	if(!res) {
 		console.log('No data found for guild: ' + guild + ', event: ' + event);
 		return;
